ebcae4ca24298aa67069b2dddae8b982d48fa5e6
dbetebenner <dbetebenner@gmail.com>
2013-10-14 10:08:21 -0400

    Update of 2012-2013 scripts

--- Mississippi_Data_LONG_2012_2013.R
+++ Mississippi_Data_LONG_2012_2013.R
@@ -7,95 +7,128 @@
 ### Load SGP package
 
 require(SGP)
+require(data.table)
+
+
+### Utility functions
+
+strtail <- function(s,n=1) {
+	if(n<0) substring(s,1-n)
+	else substring(s,nchar(s)-n+1)
+}
+strhead <- function(s,n) {
+	if(n<0) substr(s,1,nchar(s)+n)
+	else substr(s,1,n)
+}
 
 
 ### Load base data file
 
-my.colClasses <- c(rep("factor", 5), "numeric", "character", rep("factor", 3), rep("integer", 2), rep("factor", 4), rep("numeric", 2), rep("factor", 12))
+my.colClasses <- c(rep("character", 30))
 Mississippi_Data_LONG_2012_2013 <- read.table("Data/Base_Files/SGP_Mississippi_2012_2013.csv", sep="|", header=TRUE, quote="", comment.char="", colClasses=my.colClasses)
-Pilot_School <- read.table("Data/Base_Files/PilotDistSch.csv", sep="|", header=TRUE, quote="")
-SIG_School <- read.table("Data/Base_Files/SIGDistSch.csv", sep="|", header=TRUE, quote="")
-Teacher_Data <- read.table("Data/Base_Files/Teacher_ID_2012_2013.csv", sep="|", header=TRUE, quote="")
-
-# Create Pilot and SIG school lookup table
-
-names(Pilot_School)[1] <- "SCHOOL_NUMBER"
-Pilot_School$PILOT_SIG_SCHOOL <- factor(1, levels=1:2, labels=c("Pilot School", "SIG School"))
-Pilot_School <- Pilot_School[,c(1,4)]
-
-names(SIG_School)[1] <- "SCHOOL_NUMBER"
-SIG_School$PILOT_SIG_SCHOOL <- factor(2, levels=1:2, labels=c("Pilot School", "SIG School"))
-SIG_School <- SIG_School[,c(1,4)]
-
-Pilot_SIG_School <- rbind(Pilot_School, SIG_School)
-
-
-# Merge in Pilot_SIG School identifier
-
-Mississippi_Data_LONG_2012_2013 <- merge(Mississippi_Data_LONG_2012_2013, Pilot_SIG_School, all.x=TRUE)
-
-Teacher_Data <- Teacher_Data[,1:3]
-names(Teacher_Data)[1:3] <- c("INSTRUCTOR_NUMBER_1", "INSTRUCTOR_1_LAST_NAME", "INSTRUCTOR_1_FIRST_NAME")
-Mississippi_Data_LONG_2012_2013 <- merge(Mississippi_Data_LONG_2012_2013, Teacher_Data, all.x=TRUE)
-names(Teacher_Data)[1:3] <- c("INSTRUCTOR_NUMBER_2", "INSTRUCTOR_2_LAST_NAME", "INSTRUCTOR_2_FIRST_NAME")
-Mississippi_Data_LONG_2012_2013 <- merge(Mississippi_Data_LONG_2012_2013, Teacher_Data, all.x=TRUE)
+load("Data/Base_Files/Pilot_SIG_District_Schools.Rdata")
+Teacher_Data_2012_2013 <- data.table(read.table("Data/Base_Files/Teacher_ID_2012_2013.csv", sep="|", header=TRUE, quote="", comment.char="", colClasses=c("character", "factor", "factor")), 
+	key="INSTRUCTOR_NUMBER")
 
 
 ### Tidy up data
 
-Mississippi_Data_LONG_2012_2013$ID <- as.character(Mississippi_Data_LONG_2012_2013$ID)
-levels(Mississippi_Data_LONG_2012_2013$CONTENT_AREA)[5] <- "READING_LANGUAGE_ARTS"
-Mississippi_Data_LONG_2012_2013$CONTENT_AREA <- as.character(Mississippi_Data_LONG_2012_2013$CONTENT_AREA)
-levels(Mississippi_Data_LONG_2012_2013$YEAR) <- "2012_2013"
-Mississippi_Data_LONG_2012_2013$YEAR <- as.character(Mississippi_Data_LONG_2012_2013$YEAR)
-Mississippi_Data_LONG_2012_2013$GRADE <- as.integer(as.character(Mississippi_Data_LONG_2012_2013$GRADE))
-Mississippi_Data_LONG_2012_2013$VALID_CASE <- factor(1, levels=1:2, labels=c("VALID_CASE", "INVALID_CASE"))
-Mississippi_Data_LONG_2012_2013$VALID_CASE <- as.character(Mississippi_Data_LONG_2012_2013$VALID_CASE)
-levels(Mississippi_Data_LONG_2012_2013$ACHIEVEMENT_LEVEL) <- c("Minimal", "Basic", "Proficient", "Advanced")
-Mississippi_Data_LONG_2012_2013$ACHIEVEMENT_LEVEL <- as.ordered(Mississippi_Data_LONG_2012_2013$ACHIEVEMENT_LEVEL)
+Mississippi_Data_LONG_2012_2013$CONTENT_AREA[Mississippi_Data_LONG_2012_2013$CONTENT_AREA=="READING LANGUAGE ARTS"] <- "READING_LANGUAGE_ARTS"
+Mississippi_Data_LONG_2012_2013$YEAR <- "2012_2013"
+Mississippi_Data_LONG_2012_2013$GRADE <- as.character(as.integer(Mississippi_Data_LONG_2012_2013$GRADE))
+Mississippi_Data_LONG_2012_2013$VALID_CASE <- "VALID_CASE"
+Mississippi_Data_LONG_2012_2013$ACHIEVEMENT_LEVEL <- factor(Mississippi_Data_LONG_2012_2013$ACHIEVEMENT_LEVEL, levels=1:4, labels=c("Minimal", "Basic", "Proficient", "Advanced"), ordered=TRUE)
+Mississippi_Data_LONG_2012_2013$LAST_NAME <- as.factor(Mississippi_Data_LONG_2012_2013$LAST_NAME)
 levels(Mississippi_Data_LONG_2012_2013$LAST_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$LAST_NAME), capwords)
+Mississippi_Data_LONG_2012_2013$FIRST_NAME <- as.factor(Mississippi_Data_LONG_2012_2013$FIRST_NAME)
+Mississippi_Data_LONG_2012_2013$LAST_NAME <- factor(as.character(Mississippi_Data_LONG_2012_2013$LAST_NAME))
 levels(Mississippi_Data_LONG_2012_2013$FIRST_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$FIRST_NAME), capwords)
-levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_LAST_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_LAST_NAME), capwords)
-levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_LAST_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_LAST_NAME), capwords)
-levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_FIRST_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_FIRST_NAME), capwords)
-levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_FIRST_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_FIRST_NAME), capwords)
-Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_ENROLLMENT_STATUS <- factor(1, levels=1:2, labels=c("Enrolled Instructor: Yes", "Enrolled Instructor: No"))
-Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_ENROLLMENT_STATUS <- factor(1, levels=1:2, labels=c("Enrolled Instructor: Yes", "Enrolled Instructor: No"))
+levels(Mississippi_Data_LONG_2012_2013$FIRST_NAME)[3] <- "Nicholas"
+Mississippi_Data_LONG_2012_2013$FIRST_NAME <- factor(as.character(Mississippi_Data_LONG_2012_2013$FIRST_NAME))
+Mississippi_Data_LONG_2012_2013$SCHOOL_NAME <- as.factor(Mississippi_Data_LONG_2012_2013$SCHOOL_NAME)
+levels(Mississippi_Data_LONG_2012_2013$SCHOOL_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$SCHOOL_NAME), capwords)
+Mississippi_Data_LONG_2012_2013$DISTRICT_NAME <- as.factor(Mississippi_Data_LONG_2012_2013$DISTRICT_NAME)
+levels(Mississippi_Data_LONG_2012_2013$DISTRICT_NAME) <- sapply(levels(Mississippi_Data_LONG_2012_2013$DISTRICT_NAME), capwords)
 Mississippi_Data_LONG_2012_2013$STATE_ENROLLMENT_STATUS <- factor(1, levels=1:2, labels=c("Enrolled State: Yes", "Enrolled State: No"))
 Mississippi_Data_LONG_2012_2013$DISTRICT_ENROLLMENT_STATUS <- factor(1, levels=1:2, labels=c("Enrolled District: Yes", "Enrolled District: No"))
 Mississippi_Data_LONG_2012_2013$SCHOOL_ENROLLMENT_STATUS <- factor(1, levels=1:2, labels=c("Enrolled School: Yes", "Enrolled School: No"))
+Mississippi_Data_LONG_2012_2013$GENDER <- as.factor(Mississippi_Data_LONG_2012_2013$GENDER)
 levels(Mississippi_Data_LONG_2012_2013$GENDER) <- c("Female", "Male")
-levels(Mississippi_Data_LONG_2012_2013$ETHNICITY) <- c("Asian", "African American", "Hispanic", "Mixed Race", "No Response", "Pacific Islander", "White")
+Mississippi_Data_LONG_2012_2013$ETHNICITY <- NA
+Mississippi_Data_LONG_2012_2013$ETHNICITY <- as.factor(Mississippi_Data_LONG_2012_2013$ETHNICITY)
 Mississippi_Data_LONG_2012_2013$FREE_REDUCED_LUNCH_STATUS[Mississippi_Data_LONG_2012_2013$FREE_REDUCED_LUNCH_STATUS==""] <- NA
-Mississippi_Data_LONG_2012_2013$FREE_REDUCED_LUNCH_STATUS <- factor(Mississippi_Data_LONG_2012_2013$FREE_REDUCED_LUNCH_STATUS)
+Mississippi_Data_LONG_2012_2013$FREE_REDUCED_LUNCH_STATUS <- as.factor(Mississippi_Data_LONG_2012_2013$FREE_REDUCED_LUNCH_STATUS)
 levels(Mississippi_Data_LONG_2012_2013$FREE_REDUCED_LUNCH_STATUS) <- c("Free Lunch", "Paid Lunch", "Reduced Price Lunch")
+Mississippi_Data_LONG_2012_2013$IEP_STATUS <- as.factor(Mississippi_Data_LONG_2012_2013$IEP_STATUS)
 levels(Mississippi_Data_LONG_2012_2013$IEP_STATUS) <- c("IEP: No", "IEP: Yes")
+Mississippi_Data_LONG_2012_2013$ELL_STATUS <- as.factor(Mississippi_Data_LONG_2012_2013$ELL_STATUS)
 levels(Mississippi_Data_LONG_2012_2013$ELL_STATUS) <- c("ELL: No", "ELL: Yes")
-Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_WEIGHT[!is.na(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_1)] <- 1L
-Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_WEIGHT[!is.na(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_2)] <- 1L
+Mississippi_Data_LONG_2012_2013$SCALE_SCORE <- as.numeric(Mississippi_Data_LONG_2012_2013$SCALE_SCORE)
+Mississippi_Data_LONG_2012_2013$TEST_ADMINISTRATION <- as.factor(Mississippi_Data_LONG_2012_2013$TEST_ADMINISTRATION)
+
+
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_ENROLLMENT_STATUS <- NULL
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_ENROLLMENT_STATUS <- NULL
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_1_WEIGHT <- NULL
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_2_WEIGHT <- NULL
 Mississippi_Data_LONG_2012_2013$GIFTED_AND_TALENTED <- NULL
 Mississippi_Data_LONG_2012_2013$EMH_LEVEL <- NULL
+Mississippi_Data_LONG_2012_2013$ETHNICITY <- NULL
+
+# Merge in Pilot_SIG School identifier
+
+Mississippi_Data_LONG_2012_2013 <- merge(Mississippi_Data_LONG_2012_2013, Pilot_SIG_District_Schools, all.x=TRUE)
+
+# Construct Teacher data
+
+INSTRUCTOR_NUMBER <- data.table(
+		ID=rep(Mississippi_Data_LONG_2012_2013$ID, 2),
+		CONTENT_AREA=rep(Mississippi_Data_LONG_2012_2013$CONTENT_AREA, 2),
+		YEAR=rep(Mississippi_Data_LONG_2012_2013$YEAR, 2),
+		INSTRUCTOR_NUMBER=c(Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_1, Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_2),
+		INSTRUCTOR_ENROLLMENT_STATUS=factor(1, levels=1:2, labels=c("Enrolled Instructor: Yes", "Enrolled Instructor: No")),
+		INSTRUCTOR_WEIGHT=1L, key="INSTRUCTOR_NUMBER")
+
+INSTRUCTOR_NUMBER <- subset(INSTRUCTOR_NUMBER, INSTRUCTOR_NUMBER!="")
+setnames(Teacher_Data_2012_2013, c("First.Name", "Last.Name"), c("INSTRUCTOR_FIRST_NAME", "INSTRUCTOR_LAST_NAME")) 
+INSTRUCTOR_NUMBER <- Teacher_Data_2012_2013[INSTRUCTOR_NUMBER]
+INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME[INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME==""] <- NA
+INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME[INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME==""] <- NA
+INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME <- factor(INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME)
+INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME <- factor(INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME)
+INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME <- as.factor(INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME)
+levels(INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME) <- sapply(levels(INSTRUCTOR_NUMBER$INSTRUCTOR_LAST_NAME), capwords)
+INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME <- as.factor(INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME)
+levels(INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME) <- sapply(levels(INSTRUCTOR_NUMBER$INSTRUCTOR_FIRST_NAME), capwords)
+setkey(INSTRUCTOR_NUMBER, ID, CONTENT_AREA, YEAR)
+
+Instructor_Data_LONG_2012_2013 <- INSTRUCTOR_NUMBER
+save(Instructor_Data_LONG_2012_2013, file="Data/Instructor_Data_LONG_2012_2013.Rdata")
+
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_1 <- NULL
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_2 <- NULL
 
 
 ###
 ### Invalidate cases
 ###
 
-Mississippi_Data_LONG_2012_2013$VALID_CASE[Mississippi_Data_LONG_2012_2013$CONTENT_AREA=="BIOLOGY" & Mississippi_Data_LONG_2012_2013$YEAR=="2012_2013" & Mississippi_Data_LONG_2012_2013$SCALE_SCORE < 500] <- "INVALID_CASE"
 Mississippi_Data_LONG_2012_2013 <- as.data.table(Mississippi_Data_LONG_2012_2013)
 setkeyv(Mississippi_Data_LONG_2012_2013, c("VALID_CASE", "YEAR", "CONTENT_AREA", "ID", "GRADE", "SCALE_SCORE"))
 setkeyv(Mississippi_Data_LONG_2012_2013, c("VALID_CASE", "YEAR", "CONTENT_AREA", "ID"))
-invisible(Mississippi_Data_LONG_2012_2013[which(duplicated(Mississippi_Data_LONG_2012_2013))-1, VALID_CASE := "INVALID_CASE"])
+Mississippi_Data_LONG_2012_2013[which(duplicated(Mississippi_Data_LONG_2012_2013))-1, VALID_CASE := "INVALID_CASE"]
 Mississippi_Data_LONG_2012_2013 <- as.data.frame(Mississippi_Data_LONG_2012_2013)
 
 
 ### Tidy up order of variables
 
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_1 <- NULL
+Mississippi_Data_LONG_2012_2013$INSTRUCTOR_NUMBER_2 <- NULL
+
+
 preferred.variable.order <- c("ID", "CONTENT_AREA", "TEST_ADMINISTRATION", "YEAR", "GRADE", "SCALE_SCORE", "ACHIEVEMENT_LEVEL", "LAST_NAME", "FIRST_NAME",
 				"SCHOOL_NUMBER", "SCHOOL_NAME", "DISTRICT_NUMBER", "DISTRICT_NAME", 
-				"INSTRUCTOR_NUMBER_2", "INSTRUCTOR_NUMBER_1", "INSTRUCTOR_1_LAST_NAME", "INSTRUCTOR_1_FIRST_NAME", "INSTRUCTOR_2_LAST_NAME", "INSTRUCTOR_2_FIRST_NAME",
-				"INSTRUCTOR_1_WEIGHT", "INSTRUCTOR_2_WEIGHT", "INSTRUCTOR_1_ENROLLMENT_STATUS", "INSTRUCTOR_2_ENROLLMENT_STATUS",
-				"STATE_ENROLLMENT_STATUS", "DISTRICT_ENROLLMENT_STATUS", "SCHOOL_ENROLLMENT_STATUS", "GENDER", "ETHNICITY",
+				"STATE_ENROLLMENT_STATUS", "DISTRICT_ENROLLMENT_STATUS", "SCHOOL_ENROLLMENT_STATUS", "GENDER",
 				"FREE_REDUCED_LUNCH_STATUS", "IEP_STATUS", "ELL_STATUS", "PILOT_SIG_SCHOOL", "VALID_CASE")
 
 Mississippi_Data_LONG_2012_2013 <- Mississippi_Data_LONG_2012_2013[,preferred.variable.order]
